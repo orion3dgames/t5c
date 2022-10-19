dcacd054de4f94fa2e481383dc89a2b02a3111ea
Orion Gunning <orion.gunning@gmail.com>
2022-10-19 15:34:00 +1100

    save

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * IMPORTANT:
+ * ---------
+ * Do not manually edit this file if you'd like to use Colyseus Arena
+ *
+ * If you're self-hosting (without Arena), you can manually instantiate a
+ * Colyseus Server as documented here: 👉 https://docs.colyseus.io/server/api/#constructor-options
+ */
+const arena_1 = require("@colyseus/arena");
+// Import arena config
+const arena_config_1 = require("./arena.config");
+// Create and listen on 2567 (or PORT environment variable.)
+arena_1.listen(arena_config_1.default);
+//# sourceMappingURL=index.js.map
