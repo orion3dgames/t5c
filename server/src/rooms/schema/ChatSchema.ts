f197273be8c886af7aacdec8f77bf0e6240db83b
Orion Gunning <orion.gunning@gmail.com>
2022-11-13 08:49:19 +1100

    save

--- server/src/rooms/schema/ChatSchema.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, type } from "@colyseus/schema";
-
-//Chat related schemas
-export class ChatSchema extends Schema {
-  @type("string") senderID: string = "";
-  @type("string") message: string = "";
-  @type("number") timestamp: number = 0.0;
-  @type("number") createdAt: number = Date.now();
-}
