921ac43e227d284694f5eece3608aa24d83a568f
Orion Gunning <orion.gunning@gmail.com>
2022-11-12 12:27:34 +1100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@
 import { Server } from "colyseus";
 import { createServer } from "http";
 import express from "express";
+import cors from "cors";
 
 import { MyRoom } from "./rooms/MyRoom";
 import { LobbyRoom } from "@colyseus/core";
@@ -9,6 +10,7 @@ import { LobbyRoom } from "@colyseus/core";
 const port = Number(process.env.port) || 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const gameServer = new Server({
