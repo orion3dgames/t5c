fab2db706a0d8c5ac3c5cc344867c33fc4fb8aa0
Orion Gunning <orion.gunning@gmail.com>
2023-01-09 22:56:09 +1100

    Update Config.ts

--- src/shared/Config.ts
+++ src/shared/Config.ts
@@ -99,7 +99,7 @@ let Config = {
     // functions
     setDefault(){
         global.T5C = {
-            nextScene: State.GAME,
+            nextScene: State.LOGIN,
             currentRoomID: "",
             currentSessionID: "",
             currentLocation: Config.locations[Config.initialLocation],
