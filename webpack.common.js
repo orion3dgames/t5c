2b20b505d2129d2576a76866a07a25ae5a208d3c
Orion Gunning <orion.gunning@gmail.com>
2023-01-17 16:54:44 +1100

    minifying the hell out this thing :) 24mo to 2.5mo bundle

--- webpack.common.js
+++ webpack.common.js
@@ -2,19 +2,15 @@ const path = require("path");
 const fs = require('fs');
 const appDirectory = fs.realpathSync(process.cwd());
 const CopyPlugin = require("copy-webpack-plugin");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: path.resolve(appDirectory, "src/client/index.ts"),
-    devtool: "source-map",
     output: {
-        //name for the js file that is created/compiled in memory
         filename: 'js/bundle.js',
-        clean: true, // Clean the output directory before emit.
+        clean: true,
         path: path.resolve(__dirname, 'dist/client'),
     },
     resolve: {
-        // extensions: [".ts"]
         extensions: [".tsx", ".ts", ".js"],
         fallback: { 
             "console": false,
@@ -25,28 +21,17 @@ module.exports = {
             '@shared': path.resolve(__dirname, '../src/shared'),
         }
     },
-    devServer: {
-        host: '0.0.0.0',
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        watchFiles: ['src/**/*.ts', 'public/**/*'],
-        liveReload: true,
-        port: 8080,
-    },
     module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "ts-loader",
-                    options: {
-                        //sourceMap: true,
-                    }
+        rules: [{
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "ts-loader",
+                options: {
+                    //sourceMap: true,
                 }
             }
-        ]
+        }]
     },
     plugins: [
         new CopyPlugin({
