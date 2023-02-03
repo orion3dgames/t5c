6ba09dbab6da8beb98f0b1a316695787664c01c6
Orion Gunning <orion.gunning@gmail.com>
2023-02-03 14:03:04 +1100

    load testing is nearly working
    now to make the bots move around

---
+++ src/server/loadtest/test.ts
@@ -0,0 +1,61 @@
+import { Client, Room } from "colyseus.js";
+import { Options } from "@colyseus/loadtest";
+
+import { node_http } from "../../shared/Utils";
+import Config from "../../shared/Config";
+
+function findCurrentRoom(currentRoomKey):Promise<any> {
+    return new Promise(async (resolve: any, reject: any) => {
+        let rooms = await this._client.getAvailableRooms("game_room");
+        if(rooms.length > 0){
+            rooms.forEach((room) => {
+                if(room.metadata.location === currentRoomKey){ 
+                    resolve(room);
+                }
+            });
+        }
+        resolve(false);
+    });
+}
+
+export async function main(options: Options) {
+    const client = new Client(options.endpoint);
+
+    // get room
+    let foundRoom;
+    let rooms = await client.getAvailableRooms("game_room");
+    if(rooms.length > 0){
+        rooms.forEach((r) => {
+            if(r.metadata.location === 'lh_town'){ 
+                foundRoom = r;
+            }
+        });
+    }
+
+    // get random user 
+    let req = await node_http(Config.apiUrlLocal+'/returnRandomUser');
+    let character = req.user;
+
+    // join room
+    const room: Room = await client.joinById(foundRoom.roomId, { 
+        token: character.token,
+        character_id: character.id
+    });
+
+    console.log("joined successfully!", options);
+
+    //
+    room.onMessage("message-type", (payload) => {
+        // logic
+    });
+
+    //
+    room.onStateChange((state) => {
+        console.log("state change:", state);
+    });
+
+    //
+    room.onLeave((code) => {
+        console.log("left");
+    });
+}
