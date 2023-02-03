3f2276a7a3797e0bc80851408393f4d443817f2f
Orion Gunning <orion.gunning@gmail.com>
2023-02-03 14:06:19 +1100

    levelling progress should be working now

--- src/shared/Entities/Player/Leveling.ts
+++ src/shared/Entities/Player/Leveling.ts
@@ -1,3 +1,8 @@
+/**
+ * Happy to have experience as set numbers as seen below.
+ * Dayd, if you want to make something more dynamic, go ahead ;)
+ */
+
 const LEVEL_EXPERIENCE = [
     0,
     1000,
@@ -26,11 +31,9 @@ export class Leveling {
 
     public static getLevelProgress(experience) {
         let currentLevel = this.convertXpToLevel(experience);
-        let xpEarned = experience;
-        let xpMin = LEVEL_EXPERIENCE[currentLevel-1];
-        let xpMax = LEVEL_EXPERIENCE[currentLevel];
-        // daydd a toi de bosser la, il faut retourner le pourcentage pour que je puisse afficher la progression du level
-        //console.log((xpEarned/xpMin) - (xpMax-xpMin) * 100);
-    }
+        let xpEarnedThisLevel = experience - LEVEL_EXPERIENCE[currentLevel - 1];
+        let xpThisLevel = LEVEL_EXPERIENCE[currentLevel] - LEVEL_EXPERIENCE[currentLevel - 1];
+        return xpEarnedThisLevel / xpThisLevel * 100;
+      }
 
 }
