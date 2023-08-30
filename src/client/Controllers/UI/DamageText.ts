860146a6f5643846980e05e23dff4c56eb206200
Orion Gunning <orion.gunning@gmail.com>
2023-08-30 16:46:26 +1000

    save

--- src/client/Controllers/UI/DamageText.ts
+++ src/client/Controllers/UI/DamageText.ts
@@ -27,7 +27,7 @@ export class DamageText {
     }
 
     public addDamage(entity, amount) {
-        let color = amount < 0 ? "red" : "green";
+        let color = amount < 0 ? "red" : "yellowgreen";
         let amountText = amount > 0 ? "+" + amount : "" + amount;
         let key = this.damageText.length;
         var rect1 = new Rectangle("damage_" + key + "_" + entity.sessionId);
@@ -38,12 +38,12 @@ export class DamageText {
         rect1.zIndex = this._ui.addControl(rect1);
         rect1.linkWithMesh(entity.mesh);
         rect1.linkOffsetY = -50;
-        rect1.metadata = { offset: randomNumberInRange(-1, 1) };
+        rect1.metadata = { offset: randomNumberInRange(-0.5, 0.5) };
         var label = new TextBlock("text_" + entity.sessionId);
         label.text = amountText;
         label.color = color;
         label.fontWeight = "bold";
-        label.fontSize = "18px";
+        label.fontSize = "22px";
         label.outlineWidth = 3;
         label.outlineColor = "black";
         rect1.addControl(label);
@@ -51,10 +51,14 @@ export class DamageText {
     }
 
     private _update() {
+        let alphaIncrement = 0.05;
         this.damageText.forEach((v, k) => {
             v.linkOffsetYInPixels -= 2;
             v.linkOffsetXInPixels -= v.metadata.offset;
-            if (v.linkOffsetYInPixels < -150) {
+            if (v.linkOffsetYInPixels < -130 && v.alpha >= alphaIncrement) {
+                v.alpha -= alphaIncrement;
+            }
+            if (v.linkOffsetYInPixels < -250) {
                 v.dispose();
                 delete this.damageText[k];
             }
